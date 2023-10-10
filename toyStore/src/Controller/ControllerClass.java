6655a5766fd3f19a2fc28effc57c3a05bd6cec3f
Aleksei Zelyanin <magik.xrenius@gmail.com>
2023-10-10 21:13:37 +0300

    Изменение меню

--- toyStore/src/Controller/ControllerClass.java
+++ toyStore/src/Controller/ControllerClass.java
@@ -9,41 +9,41 @@ import Model.Toy;
 import View.Menu;
 
 public class ControllerClass {
-    
+
     public static void runApp() throws IOException {
         Menu menuGen = new Menu();
         PriorityQueue<Toy> listToys = new PriorityQueue<>(Comparator.comparingInt(Toy::getWeight));
         boolean appRun = true;
-        
-        while(appRun)
-        {
-        menuGen.printGeneralMenu();
-        int userChoice = menuGen.Choice(5);
-        switch (userChoice) {
-            case 1:
-                Launch.launchRun(listToys);
-                break;
-            
-            case 2:
-                listToys.add(Toy.createNewToy());
-                break;
-            case 3:
-                for (Toy toy : listToys) {
-                    System.out.println(toy);
-                }
-                break;            
-            case 4:
-                
-                break;
-            case 5:
-                appRun = false;
-                break;
-            
-            default:
-                break;
+
+        while (appRun) {
+            menuGen.printGeneralMenu();
+            int userChoice = menuGen.Choice(5);
+            switch (userChoice) {
+                case 1:
+                    Launch.launchRun(listToys);
+                    break;
+
+                case 2:
+                    listToys.add(Toy.createNewToy());
+                    break;
+                case 3:
+                    for (Toy toy : listToys) {
+                        System.out.println(toy);
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i < 10; i++) {
+                        Launch.launchRun(listToys);
+                    }
+                    break;
+                case 5:
+                    appRun = false;
+                    break;
+                default:
+                    break;
+            }
+
         }
 
     }
-    
-}
 }
--- toyStore/src/Model/Launch.java
+++ toyStore/src/Model/Launch.java
@@ -1,35 +1,61 @@
-    package Model;
+package Model;
 
-    import java.io.FileWriter;
-    import java.io.IOException;
-    import java.util.PriorityQueue;
-    import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Random;
 
-    public class Launch {
-        public static void launchRun(PriorityQueue<Toy> listToys) throws IOException {
-            FileWriter writer = new FileWriter("results.txt");
-            try {
-                Random random = new Random();
+/**
+ * Класс содержащий метод,
+ * выполняющий основную логику розыгрыша.
+ */
+public class Launch {
+    /**
+     * @param listToys - список игрушек
+     * @throws IOException - обработка ошибки и вывод трассировки стека ошибки.
+     */
+    public static void launchRun(PriorityQueue<Toy> listToys) throws IOException {
+        FileWriter writer = new FileWriter("results.txt",  true);
+        try {
+            Random random = new Random();
 
-                for (int i = 0; i < 10; i++) {
-                    int randomValue = random.nextInt(10); // Генерируем случайное число от 0 до 9
-                    Toy toy = listToys.poll(); // Извлекаем игрушку с наименьшим весом
-                    listToys.add(toy); // Помещаем её обратно в очередь
+            for (int i = 0; i < 10; i++) {
+                int randomValue = random.nextInt(10); // Генерируем случайное число от 0 до 9
 
-                    String result;
-                    if (randomValue < 2) { // 20% вероятность
-                        result = "Get: 1 - " + toy.getNameToy();
-                    } else if (randomValue < 4) { // 20% вероятность
-                        result = "Get: 2 - " + toy.getNameToy();
-                    } else { // 60% вероятность
-                        result = "Get: 3 - " + toy.getNameToy();
-                    }
-
-                    writer.write(result + "\n");
+                Toy selectedToy;
+                if (randomValue < 2) { // 20% вероятность
+                    selectedToy = selectToyWithWeight(listToys, 2);
+                } else if (randomValue < 4) { // 20% вероятность
+                    selectedToy = selectToyWithWeight(listToys, 3);
+                } else { // 60% вероятность
+                    selectedToy = selectToyWithWeight(listToys, 1);
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+
+                if (selectedToy != null) {
+                    writer.write("Get: " + selectedToy.id + " - " + selectedToy.getNameToy() + "\n");
+                    listToys.add(selectedToy); // Помещаем игрушку обратно в очередь
+                }
             }
+
             writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    /**
+     * @param toyQueue - очередь игрушек
+     * @param weight   - вес (частота выпадения) игрушки
+     * @return - возвращает toy если игрушка с указанным весом есть в очереди,
+     * возвращает null - для обозначения того, что игрушка с таким весом не была найдена.
+     */
+    private static Toy selectToyWithWeight(PriorityQueue<Toy> toyQueue, int weight) {
+        for (Toy toy : toyQueue) {
+            if (toy.weight == weight) {
+                toyQueue.remove(toy);
+                return toy;
+            }
+        }
+        return null;
+    }
+}
